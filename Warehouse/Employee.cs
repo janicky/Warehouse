afcab6f7c8d6d25d1df360b0d6cf35b740686939
Patryk Janicki <kontakt.janicki@gmail.com>
2018-11-03 12:15:32 +0100

    Dodanie konstruktora i pól dla klasy employee

--- Warehouse/Employee.cs
+++ Warehouse/Employee.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Warehouse
 {
-    public class Class1
+    public class Employee
     {
+        private static int index = 0;
+
+        private int id;
+        private string firstName;
+        private string lastName;
+
+        public Employee(string firstName, string lastName) {
+            id = index++;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
     }
 }
