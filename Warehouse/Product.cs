3185f56c0b4014d185123f06b026f36a976ada47
Patryk Janicki <kontakt.janicki@gmail.com>
2018-11-03 12:26:02 +0100

    Dodanie pól do klasy Product

--- Warehouse/Product.cs
+++ Warehouse/Product.cs
@@ -7,5 +7,17 @@ using System.Threading.Tasks;
 namespace Warehouse {
     class Product {
 
+        private static int index = 0;
+
+        private int id;
+        private string name;
+        private double weight;
+
+        public Product(string name, double weight) {
+            id = index++;
+            this.name = name;
+            this.weight = weight;
+        }
+
     }
 }
