7f40e8814f835d8f484f748cbe16bf162ade6059
Patryk Janicki <kontakt.janicki@gmail.com>
2018-11-21 19:08:31 +0100

    Dodanie projektu dla testów jednostkowych

---
+++ Warehouse.UnitTests/ProductTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warehouse.UnitTests {
+    [TestClass]
+    public class ProductTests {
+        [TestMethod]
+        public void TestMethod1() {
+            // Arrange
+            var product = new Product("test", 1.2);
+        }
+    }
+}
---
+++ Warehouse.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Warehouse.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Warehouse.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4338d510-140e-4e8f-ba02-b2afbe88acb3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
