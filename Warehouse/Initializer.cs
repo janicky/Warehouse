89b9b8e8e71471916421b3d646a6573a2b4fa54c
Patryk Janicki <kontakt.janicki@gmail.com>
2018-11-28 21:37:21 +0100

    Dodanie klasy Initializer

---
+++ Warehouse/Initializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse {
+    class Initializer {
+        public Initializer() {
+            DataService dataService = new DataService();
+            new DataRepository(dataService);
+        }
+    }
+}
