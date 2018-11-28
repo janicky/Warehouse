b5cec2947197b95d0451b1692cb6e8d0548b64aa
Patryk Janicki <kontakt.janicki@gmail.com>
2018-11-28 20:18:11 +0100

    Dodanie podstawowego testu klasy ProductCopy

---
+++ Warehouse.UnitTests/ProductCopyTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warehouse.UnitTests {
+    [TestClass]
+    public class ProductCopyTests {
+
+        private ProductCopy productCopy;
+        private double price = 62.0;
+        private string description = "Short product description";
+
+        [TestInitialize]
+        public void TestInitialize() {
+            Product product = new Product("Product name", 10.0);
+            productCopy = new ProductCopy(product, description, price, 1);
+        }
+        [TestMethod]
+        public void CreateInstance_TestMethods() {
+            // Arrange
+            // ...
+
+            // Assert
+            Assert.AreEqual(productCopy.getDescription(), description);
+            Assert.AreEqual(productCopy.getPrice(), price);
+            Assert.AreEqual(productCopy.getCount(), 1);
+        }
+    }
+}
