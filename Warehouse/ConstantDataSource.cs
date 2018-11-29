df65b1645401146c0f5a233b935937c73ad5de1f
Patryk Janicki <kontakt.janicki@gmail.com>
2018-11-29 18:45:51 +0100

    Implementacja interfejsu IDataSource w klasie ConstantDataSource

--- Warehouse/ConstantDataSource.cs
+++ Warehouse/ConstantDataSource.cs
@@ -1,13 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Warehouse
 {
-    public class ConstantDataSource : IDataSource
-    {
+    public class ConstantDataSource : IDataSource {
+        public List<Employee> GetEmployees() {
+            throw new NotImplementedException();
+        }
 
+        public ObservableCollection<Order> GetOrders() {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<int, Product> GetProducts() {
+            throw new NotImplementedException();
+        }
+
+        public ObservableCollection<ProductCopy> productCopies() {
+            throw new NotImplementedException();
+        }
     }
 }
