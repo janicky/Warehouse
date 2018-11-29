f491bc1126bf4877c49188b4d38208575ec73f6f
Patryk Janicki <kontakt.janicki@gmail.com>
2018-11-29 23:18:45 +0100

    Dodanie testowego źródła danych

---
+++ Warehouse.UnitTests/TestDataSource.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Warehouse.UnitTests {
+    class TestDataSource : IDataSource {
+        private List<Employee> employees = new List<Employee>();
+        private Dictionary<int, Product> products = new Dictionary<int, Product>();
+        private ObservableCollection<ProductCopy> productCopies = new ObservableCollection<ProductCopy>();
+        private ObservableCollection<Order> orders = new ObservableCollection<Order>();
+
+        public TestDataSource() {
+            FillEmployees();
+            FillProducts();
+            FillProductCopies();
+            FillOrders();
+        }
+
+        // Data fill
+        private void FillEmployees() {
+            employees.Add(new Employee("Anakin", "Skywalker"));
+        }
+
+        private void FillProducts() {
+            products.Add(1, new Product("Monitor", 4.5));
+        }
+
+        private void FillProductCopies() {
+            productCopies.Add(new ProductCopy(products[1], "Opis 1", 12.2, 1));
+        }
+
+        private void FillOrders() {
+            orders.Add(new Order(employees[0], productCopies[0], 12.0, 1));
+        }
+
+        // Getters
+        public List<Employee> GetEmployees() {
+            return employees;
+        }
+
+        public Dictionary<int, Product> GetProducts() {
+            return products;
+        }
+
+        public ObservableCollection<ProductCopy> GetProductCopies() {
+            return productCopies;
+        }
+
+        public ObservableCollection<Order> GetOrders() {
+            return orders;
+        }
+    }
+}
