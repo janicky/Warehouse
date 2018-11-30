f25fb9cd505710e9e441f5591486b4cdbad128bb
KJachimstal <k,jachimstal@gmail.com>
2018-11-30 19:27:08 +0100

    Dodanie testu GetProductCopyByIndex

--- Warehouse.UnitTests/DataRepositoryTests/ProductCopiesRepositoryTest.cs
+++ Warehouse.UnitTests/DataRepositoryTests/ProductCopiesRepositoryTest.cs
@@ -10,5 +10,19 @@ namespace Warehouse.UnitTests.DataRepositoryTests {
         public void TestInitialize() {
             dataRepository = new DataRepository(new TestDataSource());
         }
+
+        [TestMethod]
+        public void DataRepositoryProductCopies_GetProductCopyByIndex() {
+            try {
+                //Arrange
+                ProductCopy productCopyFromDataSource = dataRepository.GetAllProductCopies()[0];
+                ProductCopy productCopy = dataRepository.GetProductCopyByIndex(0);
+                //Assert
+                Assert.AreEqual(productCopy, productCopyFromDataSource);
+            }
+            catch {
+                Assert.Fail();
+            }
+        }
     }
 }
