e9fdba34e4d49a56e7054683ba8d3c0496ef73ad
Patryk Janicki <kontakt.janicki@gmail.com>
2018-11-30 16:59:52 +0100

    Dodanie metody GetOrderByIndex oraz GetAllOrders do klasy DataRepository

--- Warehouse/DataRepository/OrdersRepository.cs
+++ Warehouse/DataRepository/OrdersRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,18 @@ namespace Warehouse {
         // CRUD methods for Orders in this partial class
         public void AddOrder(Order order) {
             dataContext.orders.Add(order);
+        }
+
+        public Order GetOrderByIndex(int index) {
+            try {
+                return dataContext.orders.ElementAt(index);
+            } catch (Exception) {
+                return null;
+            }
+        }
+
+        public ObservableCollection<Order> GetAllOrders() {
+            return dataContext.orders;
         }
     }
 }
