c212cb1884ccf799ed6ab15484d19778bd86dc77
KJachimstal <k,jachimstal@gmail.com>
2018-11-30 17:26:40 +0100

    Inicjalizacja klasy ProductsRepositoryTest

---
+++ Warehouse.UnitTests/DataRepositoryTests/ProductsRepositoryTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Warehouse.UnitTests.DataRepositoryTests {
+    [TestClass]
+    class ProductsRepositoryTest {
+        private DataRepository dataRepository;
+
+        [TestInitialize]
+        public void TestInitialize() {
+            dataRepository = new DataRepository(new TestDataSource());
+        }
+
+        
+    }
+}
