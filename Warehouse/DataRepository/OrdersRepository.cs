f080e3d21ea324b31a518e0158f032f7223e356a
Patryk Janicki <kontakt.janicki@gmail.com>
2018-11-30 16:58:01 +0100

    Inicjalizacja CRUD dla klasy Order

---
+++ Warehouse/DataRepository/OrdersRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse {
+    public partial class DataRepository {
+        // CRUD methods for Orders in this partial class
+        public void AddOrder(Order order) {
+            dataContext.orders.Add(order);
+        }
+    }
+}
