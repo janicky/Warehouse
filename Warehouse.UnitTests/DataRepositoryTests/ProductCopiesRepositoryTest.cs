c3b0215c52d28fdc6b98c0ae309bd625788e4bb9
KJachimstal <k,jachimstal@gmail.com>
2018-11-30 20:30:30 +0100

    Korekcja testów jednostkowych w klasie ProductCopiesRepositoryTest

--- Warehouse.UnitTests/DataRepositoryTests/ProductCopiesRepositoryTest.cs
+++ Warehouse.UnitTests/DataRepositoryTests/ProductCopiesRepositoryTest.cs
@@ -27,11 +27,16 @@ namespace Warehouse.UnitTests.DataRepositoryTests {
 
         [TestMethod]
         public void DataRepositoryProductCopies_GetProductCopyById() {
-            //Arrenge
-            ProductCopy productCopyFromDataSource = dataRepository.GetAllProductCopies()[0];
-            ProductCopy productCopy = dataRepository.GetProductCopyByIndex(0);
-            //Assert
-            Assert.AreEqual(productCopy, productCopyFromDataSource);
+            try {
+                //Arrange
+                ProductCopy productCopyFromDataSource = dataRepository.GetAllProductCopies()[0];
+                ProductCopy productCopy = dataRepository.GetProductCopyByIndex(0);
+                //Assert
+                Assert.AreEqual(productCopy, productCopyFromDataSource);
+            }
+            catch {
+                Assert.Fail();
+            }
         }
     }
 }
