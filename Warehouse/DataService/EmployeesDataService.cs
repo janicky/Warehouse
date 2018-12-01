4edef9fc2abb2f837c7faae049fcdfce64ebdb85
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-01 18:15:29 +0100

    Dodanie metody FindEmployee do klasy DataService

--- Warehouse/DataService/EmployeesDataService.cs
+++ Warehouse/DataService/EmployeesDataService.cs
@@ -13,23 +13,27 @@ namespace Warehouse {
             }
         }
 
+        public Employee FindEmployee(Func<Employee, bool> condition) {
+            return repository.GetAllEmployees().First(condition);
+        }
+
         public Employee AddEmployee(string firstName, string lastName) {
             Employee employee = new Employee(firstName, lastName);
             repository.AddEmployee(employee);
             return employee;
         }
 
+        public void UpdateEmployee(Employee employee) {
+            repository.UpdateEmployee(employee);
+        }
+
         public bool DeleteEmployee(Func<Employee, bool> condition) {
             try {
-                Employee employee = repository.GetAllEmployees().First(condition);
+                Employee employee = FindEmployee(condition);
                 return repository.DeleteEmployee(employee);
             } catch (Exception) {
                 return false;
             }
-        }
-
-        public void UpdateEmployee(Employee employee) {
-            repository.UpdateEmployee(employee);
         }
     }
 }
