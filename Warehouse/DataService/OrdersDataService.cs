274fb0277258546eb0418e4edf8aeb9e227688c6
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-01 19:29:20 +0100

    Inicjalizacja DataService dla klasy Order

---
+++ Warehouse/DataService/OrdersDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse {
+    public partial class DataService {
+
+        public void DisplayOrders(IEnumerable<Order> orders) {
+            foreach (Order order in orders) {
+                Console.WriteLine(order);
+            }
+        }
+    }
+}
