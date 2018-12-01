7eaaeae0ad2120eb61c71586be740f994a64e90d
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-01 14:09:05 +0100

    Dodanie testu ujemnej ceny zamówienia

--- Warehouse.UnitTests/OrderTests.cs
+++ Warehouse.UnitTests/OrderTests.cs
@@ -6,11 +6,23 @@ namespace Warehouse.UnitTests
     [TestClass]
     public class OrderTests {
         private DataRepository dataRepository;
+        private Order order;
 
         [TestInitialize]
         public void TestInitialize() {
             Order.ResetIndex();
             dataRepository = new DataRepository(new TestDataSource());
+            order = dataRepository.GetAllOrders()[0];
+        }
+
+        [TestMethod]
+        public void Order_NegativeAndNullPrice() {
+            double[] prices = { 1, 0, -0.5, -0.0000001 };
+
+            foreach (double price in prices) {
+                order.SetPrice(price);
+                Assert.IsTrue(order.GetPrice() > 0);
+            }
         }
     }
 }
