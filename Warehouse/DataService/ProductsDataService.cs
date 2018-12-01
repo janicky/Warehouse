8ed945983cef5a63dbc313648fc17788ab18bb90
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-01 18:20:29 +0100

    Inicjalizacja klasy częściowej DataService dla klasy Product

---
+++ Warehouse/DataService/ProductsDataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse {
+    public partial class DataService {
+        public void DisplayProducts(Dictionary<int, Product> products) {
+            foreach (Product product in products) {
+                Console.WriteLine(product);
+            }
+        }
+    }
+}
