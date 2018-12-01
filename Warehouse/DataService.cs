fcf6f083668072c35228bc457009659b574ea4e5
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-01 20:02:54 +0100

    Dodanie obsługi zdarzeń generowanych przez ObservableCollection

--- Warehouse/DataService.cs
+++ Warehouse/DataService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -11,20 +12,9 @@ namespace Warehouse {
         private DataRepository repository;
 
         public DataService(DataRepository repository) {
+            repository.GetAllOrders().CollectionChanged += HandleOrdersChange;
+            repository.GetAllProductCopies().CollectionChanged += HandleProductCopiesChange;
             this.repository = repository;
-        }
-
-        public void DisplayProducts(IEnumerable<Product> products) {
-            foreach (Product product in products) {
-                Console.WriteLine(product);
-                foreach (ProductCopy productCopy in product.GetProductCopies()) {
-                    Console.WriteLine(productCopy);
-                }
-            }
-        }
-
-        public Dictionary<int, Product> GetAllProducts() {
-            return repository.GetAllProducts();
         }
 
         public IEnumerable<Order> GetOrdersForEmployee(Employee employee) {
@@ -35,12 +25,36 @@ namespace Warehouse {
             return repository.GetAllOrders().Where(e => e.GetOrderDate() >= from && e.GetOrderDate() <= to);
         }
 
-        public Order AddOrder(Employee employee, ProductCopy productCopy) {
-            Order order = new Order(employee, productCopy);
-            productCopy.SetOrder(order);
-            repository.AddOrder(order);
-            return order;
+        private void HandleOrdersChange(object sender, NotifyCollectionChangedEventArgs e) {
+            Console.WriteLine("Orders list was changed!");
+            if (e.NewItems != null) {
+                Console.WriteLine(string.Format("|- Orders added: {0}", e.NewItems.Count));
+                foreach (Order order in e.NewItems) {
+                    Console.WriteLine(string.Format("|-- {0}", order));
+                }
+            }
+            if (e.OldItems != null) {
+                Console.WriteLine(string.Format("|- Orders removed: {0}", e.OldItems.Count));
+                foreach (Order order in e.OldItems) {
+                    Console.WriteLine(string.Format("|-- {0}", order));
+                }
+            }
         }
 
+        private void HandleProductCopiesChange(object sender, NotifyCollectionChangedEventArgs e) {
+            Console.WriteLine("Orders list was changed!");
+            if (e.NewItems != null) {
+                Console.WriteLine(string.Format("|- Orders added: {0}", e.NewItems.Count));
+                foreach (ProductCopy productCopy in e.NewItems) {
+                    Console.WriteLine(string.Format("|-- {0}", productCopy));
+                }
+            }
+            if (e.OldItems != null) {
+                Console.WriteLine(string.Format("|- Orders removed: {0}", e.OldItems.Count));
+                foreach (ProductCopy productCopy in e.OldItems) {
+                    Console.WriteLine(string.Format("|-- {0}", productCopy));
+                }
+            }
+        }
     }
 }
