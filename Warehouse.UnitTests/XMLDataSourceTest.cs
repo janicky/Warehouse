1af7f13536e898eabea61cccef9217f0ea980e63
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-02 19:13:57 +0100

    Dodanie testów jednostkowych dla deserializacji klasy Product

--- Warehouse.UnitTests/XMLDataSourceTest.cs
+++ Warehouse.UnitTests/XMLDataSourceTest.cs
@@ -23,5 +23,15 @@ namespace Warehouse.UnitTests {
             Employee employee = new Employee(id: 0, firstName: "Ahsoka", lastName: "Tano");
             Assert.AreEqual(employee, employees[0]);
         }
+
+        [TestMethod]
+        public void ProductsDeserialization() {
+            // Arrange
+            Dictionary<int, Product> products = dataSource.GetProducts();
+            Product product = new Product(id: 0, name: "Mother Night", weight: 23.08);
+            // Assert
+            Assert.AreEqual(products.Count, 30);
+            Assert.AreEqual(product, products[1]);
+        }
     }
 }
--- Warehouse/DataSources/XMLDataSource.cs
+++ Warehouse/DataSources/XMLDataSource.cs
@@ -16,16 +16,27 @@ namespace Warehouse {
                 return ((EmployeeList)serializer.Deserialize(reader)).Employees;
             }
         }
+    
+        public Dictionary<int, Product> GetProducts() {
+            using (XmlReader reader = XmlReader.Create("W:/C#/Warehouse/Warehouse/DataSources/DataFiles/Products.xml")) {
+                XmlSerializer serializer = new XmlSerializer(typeof(ProductList));
+
+                List<Product> products = ((ProductList)serializer.Deserialize(reader)).Products;
+                Dictionary<int, Product> dictionary = new Dictionary<int, Product>();
+
+                foreach (Product product in products) {
+                    dictionary.Add(product.Key, product);
+                }
+
+                return dictionary;
+            }
+        }
 
         public ObservableCollection<Order> GetOrders() {
             throw new NotImplementedException();
         }
 
         public ObservableCollection<ProductCopy> GetProductCopies() {
-            throw new NotImplementedException();
-        }
-
-        public Dictionary<int, Product> GetProducts() {
             throw new NotImplementedException();
         }
     }
