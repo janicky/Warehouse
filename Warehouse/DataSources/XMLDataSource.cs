a46547047ca929d264e8c9c51d472f21d2872cc6
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-02 14:51:06 +0100

    Inicjalizacja klasy XMLDataSource

---
+++ Warehouse/DataSources/XMLDataSource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse {
+    class XMLDataSource : IDataSource {
+        public List<Employee> GetEmployees() {
+            throw new NotImplementedException();
+        }
+
+        public ObservableCollection<Order> GetOrders() {
+            throw new NotImplementedException();
+        }
+
+        public ObservableCollection<ProductCopy> GetProductCopies() {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<int, Product> GetProducts() {
+            throw new NotImplementedException();
+        }
+    }
+}
