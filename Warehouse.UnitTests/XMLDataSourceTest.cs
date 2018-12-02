59141bfb413c34391015ca1454e85ca68768c35e
Patryk Janicki <kontakt.janicki@gmail.com>
2018-12-02 16:57:28 +0100

    Dodanie testów poprawności pól klasy Employee

--- Warehouse.UnitTests/XMLDataSourceTest.cs
+++ Warehouse.UnitTests/XMLDataSourceTest.cs
@@ -19,6 +19,9 @@ namespace Warehouse.UnitTests {
             List<Employee> employees = dataSource.GetEmployees();
             // Assert
             Assert.AreEqual(employees.Count, 20);
+
+            Employee employee = new Employee(id: 0, firstName: "Ahsoka", lastName: "Tano");
+            Assert.AreEqual(employee, employees[0]);
         }
     }
 }
