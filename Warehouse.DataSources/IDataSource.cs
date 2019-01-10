f4264c324130d5536f9469c99d5232a138998e1d
Patryk Janicki <kontakt.janicki@gmail.com>
2019-01-10 20:03:43 +0100

    Utworzenie nowego projektu w celu realizacji założeń Dependency Injection

---
+++ Warehouse.DataSources/IDataSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.DataSources
+{
+    public interface IDataSource {
+        List<Employee> GetEmployees();
+        Dictionary<int, Product> GetProducts();
+        ObservableCollection<ProductCopy> GetProductCopies();
+        ObservableCollection<Order> GetOrders();
+    }
+}
